f9e4685c645ff24ac9f2ff5601c3bdbc9cad1a7f
shobhit1982 <infosaiisys@gmail.com>
2015-04-16 12:53:11 +0530

    Price Table

---
+++ MLM/MLM.API.Models/Voucher/PairPriceMaster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLM.Models
+{
+   public class PairPriceMaster : MLMBaseEntity
+    {
+        public int pair { get; set; }
+
+        public int TimeinDays { get; set; }
+
+        public string Price { get; set; }
+    }
+}
