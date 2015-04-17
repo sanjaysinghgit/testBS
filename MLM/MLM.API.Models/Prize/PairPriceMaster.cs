a694b11b3d17f81d94beb31783f53f261f764d39
shobhit1982 <infosaiisys@gmail.com>
2015-04-17 14:15:38 +0530

    update Models

---
+++ MLM/MLM.API.Models/Prize/PairPriceMaster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLM.Models
+{
+   public class PairPriceMaster : MLMBaseEntity
+    {
+        public int pairFrom { get; set; }
+        public int pairTo { get; set; }
+        public int TimeinDaysFrom { get; set; }
+        public int TimeinDaysTo { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ MLM/MLM.API.Models/Prize/PairPrizeWinner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLM.Models
+{
+    public class PairPrizeWinner
+    {
+        public long Id { get; set; }
+        public long voucherid { get; set; }
+        public DateTime voucherdate { get; set; }
+        public string Agentcode { get; set; }
+        public int TotPair { get; set; }
+        public int days { get; set; }
+        public int Prize{ get; set; }
+
+    }
+}
